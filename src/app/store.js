88c056d0c0dd9d6254998163389c77c5363a9037
Guy Pollack <guypollack@Guys-MacBook-Pro.local>
2023-01-29 17:35:32 +0200

    Add pokemonSlice and async thunk to fetch pokemon names and display on home page

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,24 @@
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
+import pokemonReducer from '../features/pokemon/pokemonSlice';
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    pokemon: pokemonReducer
   },
 });
+
+
+// import { configureStore } from '@reduxjs/toolkit';
+// import counterReducer from '../features/counter/counterSlice';
+// import feedReducer from "../features/feed/feedSlice";
+// import subredditsReducer from "../features/subreddits/subredditsSlice"
+
+// export const store = configureStore({
+//   reducer: {
+//     counter: counterReducer,
+//     feed: feedReducer,
+//     subreddits: subredditsReducer
+//   },
+// });
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,13 +1,35 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { MyButton } from '../components/MyButton';
 import { NavBar } from '../components/NavBar';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchAllPokemonAsync, selectAllPokemon, selectStatus } from '../features/pokemon/pokemonSlice';
 
 export function HomePage() {
+  const dispatch = useDispatch();
+
+  const allPokemon = useSelector(selectAllPokemon);
+  const status = useSelector(selectStatus);
+
+  // dispatch(fetchAllPokemonAsync());
+
+  if (status !== "idle") {
+    dispatch(fetchAllPokemonAsync());
+  }
+
+  // useEffect(() => {
+  //   dispatch(fetchAllPokemonAsync());
+  //   // console.log(allPokemon);
+  // },[allPokemon])
+
+  // console.log(allPokemon);
+
   return (
     <div>
       <NavBar />
       <h1>Hello World!</h1>
       <h2>This is the home page</h2>
+      {status === "loading" && <h4>Data loading...</h4>}
+      {status === "idle" && allPokemon.map(pokemon => <p key={pokemon.name}>{pokemon.name} ~ {pokemon.url}</p>)}
     </div>
   )
 }
