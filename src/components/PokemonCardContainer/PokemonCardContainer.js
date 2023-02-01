364c82ff6edd8970752ad7d8f022ac9764a0c34a
Guy Pollack <guypollack@Guys-MacBook-Pro.local>
2023-02-01 18:16:48 +0200

    Add event listeners to change pokemon card image class when hovered

--- src/components/PokemonCardContainer/PokemonCardContainer.js
+++ src/components/PokemonCardContainer/PokemonCardContainer.js
@@ -6,14 +6,29 @@ export function PokemonCardContainer({number, name, imageUrl}) {
   // console.log("A");
   // console.log(number);
 
+  let imageClassName = "pokemon-card-image";
+  const id = `${name}-card-image`;
+
   const navigate = useNavigate();
 
   function handleClick() {
     navigate("/pokemon/"+name);
   }
+
+  function handleMouseOver() {
+    // imageClassName = "pokemon-card-image-hovered";
+    // alert(imageClassName);
+    document.getElementById(id).classList.add("hovered");
+
+  }
+
+  function handleMouseOut() {
+    document.getElementById(id).classList.remove("hovered");
+  }
+
   return (
-    <div key={number} className="pokemon-card-container" onClick={handleClick}>
-      <img className="pokemon-card-image" src={imageUrl} />
+    <div key={number} className="pokemon-card-container" onClick={handleClick} onMouseOver={handleMouseOver} onMouseOut={handleMouseOut} >
+      <img id={id}  className="pokemon-card-image" src={imageUrl} />
       <div className="pokemon-card-details">
         <p>{number}</p>
         <p>{name}</p>
