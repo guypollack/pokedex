a12d8429091745ea6dfdf57d70ef252487ab360b
Guy Pollack <guypollack@Guys-MacBook-Pro.local>
2023-02-10 00:08:08 +0100

    Add logic to NavBar to update number of Favourites dynamically

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { selectFavourites } from '../../features/favourites/favouritesSlice';
 import "./NavBar.css";
 
 export function NavBar() {
+  const favouritesNumber = useSelector(selectFavourites).length;
   return (
     <div className="nav-bar">
       <NavLink to='/'>Home</NavLink>
       <NavLink to='/page1'>Page 1</NavLink>
       <NavLink to='/page2'>Page 2</NavLink>
-      <NavLink to='/guest/favourites'>Favourites</NavLink>
+      <NavLink to='/guest/favourites'>Favourites{favouritesNumber > 0 ? `(${favouritesNumber})` : ""}</NavLink>
     </div>
   )
 }
