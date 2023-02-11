dcb4f295df41d43862f92beb0368658480b25f87
Guy Pollack <guypollack@Guys-MacBook-Pro.local>
2023-02-11 22:30:21 +0100

    Use image server in img src property to reduce image quality in PokemonCard.js

--- src/components/PokemonCard/PokemonCard.js
+++ src/components/PokemonCard/PokemonCard.js
@@ -10,7 +10,7 @@ export function PokemonCard({number, name, id, imageUrl, favourited, onClickCard
   return (
     <div key={number} className="pokemon-card" onClick={onClickCard}>
       <FavouriteButton favourited={favourited} onClick={onClickFavourite}/>
-      <img id={id}  className="pokemon-card-image" src={imageUrl} onMouseOver={handleMouseOver} onMouseOut={handleMouseOut} />
+      <img id={id}  className="pokemon-card-image" src={`https://images.weserv.nl/?url=${imageUrl}&w=150&h=150`} onMouseOver={handleMouseOver} onMouseOut={handleMouseOut} />
       <div className="pokemon-card-details">
         <p>{number}</p>
         <p>{name}</p>
