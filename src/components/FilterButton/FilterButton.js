b42d4845bca3a33598b1f2b14fc5b90b3aa5cd56
Guy Pollack <guypollack@Guys-MacBook-Pro.local>
2023-02-14 02:59:06 +0200

    Dispatch setFilteredPokemon when FilterButton clicked to capture snapshot of previously displayed pokemon (as done in SeeMoreButton in last commit)

--- src/components/FilterButton/FilterButton.js
+++ src/components/FilterButton/FilterButton.js
@@ -1,14 +1,17 @@
 import React from "react";
-import { addFilter, removeFilter, filterPokemonAsync, setIsFiltering, setReload } from "../../features/pokemon/pokemonSlice";
-import { useDispatch } from "react-redux";
+import { addFilter, removeFilter, filterPokemonAsync, setIsFiltering, setReload, selectFilteredPokemon, setFilteredPokemon } from "../../features/pokemon/pokemonSlice";
+import { useDispatch, useSelector } from "react-redux";
 import "./FilterButton.css"
 
 export function FilterButton({property, value}) {
   const dispatch = useDispatch();
 
+  const filteredPokemon = useSelector(selectFilteredPokemon);
+
   function handleClick(e) {
     // alert("A");
     // alert(e.target.classList.contains("active"));
+    dispatch(setFilteredPokemon(filteredPokemon));
     if (!e.target.classList.contains("active")) {
       dispatch(setIsFiltering(true));
       dispatch(setReload(true));
