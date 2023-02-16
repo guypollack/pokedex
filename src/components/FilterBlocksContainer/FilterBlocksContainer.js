1aafc0a8e944d9509db0e47f26fad07044ec7700
Guy Pollack <guypollack@Guys-MacBook-Pro.local>
2023-02-16 16:25:47 +0200

    Update styling in FilterBlocksContainer.css

--- src/components/FilterBlocksContainer/FilterBlocksContainer.js
+++ src/components/FilterBlocksContainer/FilterBlocksContainer.js
@@ -3,10 +3,13 @@ import React from "react";
 import { FilterBlock } from "../FilterBlock/FilterBlock";
 import "./FilterBlocksContainer.css";
 
-export function FilterBlocksContainer({filters}) {
+export function FilterBlocksContainer({category, filters}) {
   return (
     <div className="filter-blocks-container">
-      {filters.map(([category, value], index) => <FilterBlock property={category} value={value} filterNumber={index} key={`${category}-filter-block-${index}`}/>)}
+      <h3>{category}</h3>
+      <div className="filter-blocks-flex-container">
+        {filters.map(([cat, value], index) => <FilterBlock property={cat} value={value} filterNumber={index} key={`${cat}-filter-block-${index}`}/>)}
+      </div>
     </div>
   )
 }
