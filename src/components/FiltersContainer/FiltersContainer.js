d2812aaf230137b04b5308670b5bfce508942afe
Guy Pollack <guypollack@Guys-MacBook-Pro.local>
2023-02-16 17:34:03 +0200

    Add properties and reducers in pokemonSlice to toggle AND / OR search within and between categories, update selectFilteredPokemon to refer to these properties, add buttons to FiltersContainer to toggle them

--- src/components/FiltersContainer/FiltersContainer.js
+++ src/components/FiltersContainer/FiltersContainer.js
@@ -1,13 +1,15 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 
 import { FilterCreator } from "../FilterCreator/FilterCreator";
 import { FilterBlocksContainer } from "../FilterBlocksContainer/FilterBlocksContainer";
 import { selectFilterList } from "../../features/filters/filtersSlice";
+import { selectInCategorySearchType, selectBetweenCategorySearchType, toggleInCategorySearchType, toggleBetweenCategorySearchType } from "../../features/pokemon/pokemonSlice"; 
 import "./FiltersContainer.css";
 
 
 export function FiltersContainer() {
+  const dispatch = useDispatch();
 
   const filterList  = useSelector(selectFilterList);
   const typeFilters = filterList.filter(([category, value]) => category === "Type");
@@ -15,8 +17,13 @@ export function FiltersContainer() {
   const heightFilters = filterList.filter(([category, value]) => category === "Height");
   const weightFilters = filterList.filter(([category, value]) => category === "Weight");
 
+  const inCategorySearchType = useSelector(selectInCategorySearchType);
+  const betweenCategorySearchType = useSelector(selectBetweenCategorySearchType);
+
   return (
     <div className="filters-container">
+      <button onClick={() => dispatch(toggleInCategorySearchType())}>In category search type: {inCategorySearchType}</button>
+      <button onClick={() => dispatch(toggleBetweenCategorySearchType())}>Between category search type: {betweenCategorySearchType}</button>
       <FilterCreator />
       <div className="filter-blocks-containers-flex">
         {typeFilters.length > 0 && <FilterBlocksContainer category="type" filters={typeFilters} />}
