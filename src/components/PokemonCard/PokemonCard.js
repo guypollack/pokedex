4da020506d52b83f549e838f93fda00eef1a43ef
Guy Pollack <guypollack@Guys-MacBook-Pro.local>
2023-02-19 10:44:57 +0200

    Move FavouriteButton into own folder, separate from PokemonCard.js

--- src/components/PokemonCard/PokemonCard.js
+++ src/components/PokemonCard/PokemonCard.js
@@ -1,8 +1,5 @@
 import React from "react";
-import { FavouriteButton } from "./FavouriteButton";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHeart as outlineHeart } from "@fortawesome/free-regular-svg-icons";
-import { faHeart as solidHeart } from "@fortawesome/free-solid-svg-icons";
+import { FavouriteButton } from "../FavouriteButton/FavouriteButton";
 
 import "./PokemonCard.css";
 
