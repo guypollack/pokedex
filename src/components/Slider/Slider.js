90219305056912fd7007079dcfe3c64197666273
Guy Pollack <guypollack@Guys-MacBook-Pro.local>
2023-02-19 13:17:08 +0200

    Update Slider.css and Slider.js to remove colours from sliders and bold selected slider option

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -27,18 +27,18 @@ export function Slider({heading, type, value1, value2, checked}) {
     <div className="slider-flex-container">
       <h6>{heading}</h6>
       <div className="slider-container">
-        <div>
-          <p>{value1}</p>
-        </div>
         <input type="checkbox" id={`${heading}-checkbox`} checked={checked} onChange={() => dispatch(toggleSearchType(type))}></input>
         <label htmlFor={`${heading}-checkbox`} className="slider-bar">
           <div className="slider-button"></div>
         </label>
+        <div>
+          <p className="slider-value">{value1}</p>
+        </div>
         {/* <div className="slider-bar" onClick={handleClick}>
           <div className="slider-button"></div>
         </div> */}
         <div>
-          <p>{value2}</p>
+          <p className="slider-value">{value2}</p>
         </div>
       </div>
     </div>
