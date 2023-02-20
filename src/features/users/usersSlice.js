f05d429672ac17ccac9f2028e31553fd00dfdd71
Guy Pollack <guypollack@Guys-MacBook-Pro.local>
2023-02-20 11:36:47 +0200

    Add logic to usersSlice to create account and log in

--- src/features/users/usersSlice.js
+++ src/features/users/usersSlice.js
@@ -1,20 +1,69 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-  users: []
+  users: [],
+  warning: "",
+  successMessage: "",
+  currentUser: "",
+  loginUsername: "",
+  loginPassword: "",
+  createAccountUsername: "",
+  createAccountPassword: "",
+  createAccountPassword2: "",
 };
 
 export const usersSlice = createSlice({
   name: "users",
   initialState,
   reducers: {
-    addUser: (state, action) => {
-
+    addUser: (state) => {
+      if (state.createAccountUsername === "" || state.createAccountPassword === "" || state.createAccountPassword2 === "") {
+        state.warning = "Do not leave any fields blank";
+      } else {
+        if (state.createAccountPassword === state.createAccountPassword2) {
+          if (!state.users.map(user => user["username"]).includes(state.createAccountUsername)) {
+            state.users.push({"username": state.createAccountUsername, "password": state.createAccountPassword});
+            state.createAccountUsername = "";
+            state.createAccountPassword = "";
+            state.createAccountPassword2 = "";
+            state.warning = "";
+            state.successMessage = "Account created and logged in";
+          } else {
+            state.warning = "Username already exists";
+          }
+        } else {
+          state.warning = "Passwords don't match";
+        }
+      }
+    },
+    setCreateAccountUsername: (state, action) => {
+      state.createAccountUsername = action.payload;
+      state.warning = "";
+    },
+    setCreateAccountPassword: (state, action) => {
+      state.createAccountPassword = action.payload;
+      state.warning = "";
+    },
+    setCreateAccountPassword2: (state, action) => {
+      state.createAccountPassword2 = action.payload;
+      state.warning = "";
+    },
+    setSuccessMessage: (state, action) => {
+      state.successMessage = action.payload;
+    },
+    setCurrentUser: (state, action) => {
+      state.currentUser = action.payload;
     }
-
   }
 });
 
-export const { addUser } = usersSlice.actions;
+export const { addUser, setCurrentUser, setCreateAccountUsername, setCreateAccountPassword, setCreateAccountPassword2, setSuccessMessage } = usersSlice.actions;
+
+export const selectCurrentUser = (state) => state.users.currentUser;
+export const selectCreateAccountUsername = (state) => state.users.createAccountUsername;
+export const selectCreateAccountPassword = (state) => state.users.createAccountPassword;
+export const selectCreateAccountPassword2 = (state) => state.users.createAccountPassword2;
+export const selectWarning = (state) => state.users.warning;
+export const selectSuccessMessage = (state) => state.users.successMessage;
 
 export default usersSlice.reducer;
--- src/pages/LoginPage/LoginPage.js
+++ src/pages/LoginPage/LoginPage.js
@@ -1,7 +1,44 @@
-import React from 'react';
-import { NavBar } from '../../components/NavBar/NavBar.js'
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { NavBar } from '../../components/NavBar/NavBar.js';
+import { useNavigate } from 'react-router';
+import { selectUsers, setCurrentUser, addUser, selectCreateAccountUsername, setCreateAccountUsername, selectCreateAccountPassword, setCreateAccountPassword, selectCreateAccountPassword2, setCreateAccountPassword2, selectWarning, selectSuccessMessage, setSuccessMessage } from '../../features/users/usersSlice.js';
 
 export function LoginPage() {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const createAccountUsername = useSelector(selectCreateAccountUsername);
+  const createAccountPassword = useSelector(selectCreateAccountPassword);
+  const createAccountPassword2 = useSelector(selectCreateAccountPassword2);
+  const warning = useSelector(selectWarning);
+  const successMessage = useSelector(selectSuccessMessage);
+
+  function handleChangeCreateAccountUsername(e) {
+    dispatch(setCreateAccountUsername(e.target.value));
+  }
+
+  function handleChangeCreateAccountPassword(e) {
+    dispatch(setCreateAccountPassword(e.target.value));
+  }
+
+  function handleChangeCreateAccountPassword2(e) {
+    dispatch(setCreateAccountPassword2(e.target.value));
+  }
+
+  function handleClickCreateAccount() {
+    dispatch(addUser());
+  }
+
+  useEffect(() => {
+    if (successMessage === "Account created and logged in") {
+      setTimeout(() => {
+        dispatch(setSuccessMessage(""));
+        dispatch(setCurrentUser(createAccountUsername));
+        navigate("/");
+      },2000)
+    }
+  },[successMessage]);
+
   return (
     <div>
       <NavBar />
@@ -13,11 +50,14 @@ export function LoginPage() {
       <input type="password"></input>
       <h3>Create account</h3>
       <label htmlFor="create-account-username">Username</label>
-      <input type="text"></input>
+      <input type="text" value={createAccountUsername} onChange={handleChangeCreateAccountUsername}></input>
       <label htmlFor="create-account-password">Password</label>
-      <input type="password"></input>
+      <input type="password" value={createAccountPassword} onChange={handleChangeCreateAccountPassword}></input>
       <label htmlFor="create-account-password-very">Retype password</label>
-      <input type="password"></input>
+      <input type="password" value={createAccountPassword2} onChange={handleChangeCreateAccountPassword2}></input>
+      <button onClick={handleClickCreateAccount}>Create Account</button>
+      <p className="create-account-warning">{warning}</p>
+      <p id="success-message" className="create-account-success-message">{successMessage}</p>
     </div>
   )
 }
