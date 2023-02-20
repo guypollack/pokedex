2cd9068561e397bf89fd7d6201bfb073f9dd8ab2
Guy Pollack <guypollack@Guys-MacBook-Pro.local>
2023-02-20 22:01:21 +0200

    Display disabled heart icon to in FavouriteButton if user is not logged in

--- src/components/FavouriteButton/FavouriteButton.js
+++ src/components/FavouriteButton/FavouriteButton.js
@@ -4,7 +4,7 @@ import { toggleFavourite, selectFavourites } from "../../features/favourites/fav
 import { selectCurrentUser } from "../../features/users/usersSlice";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHeart as outlineHeart } from "@fortawesome/free-regular-svg-icons";
-import { faHeart as solidHeart } from "@fortawesome/free-solid-svg-icons";
+import { faHeart as solidHeart, faHeartCircleXmark as xmarkHeart } from "@fortawesome/free-solid-svg-icons";
 
 import "./FavouriteButton.css";
 
@@ -23,14 +23,22 @@ export function FavouriteButton({number}) {
     e.stopPropagation();
   }
 
-  return (
-    <div className="favourite-button">
-      <input type="checkbox" id={`favourite-button-${number}`} checked={favourited} onClick={handleClick} onChange={handleChange}></input>
-      <label htmlFor={`favourite-button-${number}`} onClick={handleClick} >
-        <FontAwesomeIcon className="outline-heart" icon={outlineHeart} />
-        <FontAwesomeIcon className="solid-heart" icon={solidHeart} />
-        <FontAwesomeIcon className="animation-heart" icon={solidHeart} />
-      </label>  
-    </div>
-  )
+  if (currentUser !== "guest") {
+    return (
+      <div className="favourite-button">
+        <input type="checkbox" id={`favourite-button-${number}`} checked={favourited} onClick={handleClick} onChange={handleChange}></input>
+        <label htmlFor={`favourite-button-${number}`} onClick={handleClick} >
+          <FontAwesomeIcon className="outline-heart" icon={outlineHeart} />
+          <FontAwesomeIcon className="solid-heart" icon={solidHeart} />
+          <FontAwesomeIcon className="animation-heart" icon={solidHeart} />
+        </label>  
+      </div>
+    )
+  } else {
+    return (
+      <div className="favourite-button">
+        <FontAwesomeIcon className="xmark-heart" icon={xmarkHeart} title="Login to add Pokémon to your favourites" onClick={handleClick} />
+      </div>
+    )
+  }
 };
