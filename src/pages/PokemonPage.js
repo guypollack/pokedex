c2db00c3fb18d8f23e4f885df13b0a5701bc4357
Guy Pollack <guypollack@Guys-MacBook-Pro.local>
2023-02-23 09:04:48 +0200

    Fetch basic pokemon data when PokemonPage renders

--- src/pages/PokemonPage.js
+++ src/pages/PokemonPage.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { selectAllPokemon } from '../features/pokemon/pokemonSlice.js';
 import { NavBar } from '../components/NavBar/NavBar.js'
 
 export function PokemonPage() {
   const { pokemonNumber } = useParams();
+  const {name, url, imageUrl} = useSelector(selectAllPokemon)[pokemonNumber];
+  
+  useEffect(() => {
+    console.log(name);
+  })
+
   return (
     <div>
       <NavBar />
