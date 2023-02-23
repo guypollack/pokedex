27f36ce47b0840aefd98793454e30c540db5075b
Guy Pollack <guypollack@Guys-MacBook-Pro.local>
2023-02-23 17:32:45 +0200

    Update formatting of PokemonPage to add grid

--- src/pages/PokemonPage/PokemonPage.js
+++ src/pages/PokemonPage/PokemonPage.js
@@ -4,6 +4,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import { fetchAllPokemonAsync, selectAllPokemonFetched, selectAllPokemon, selectPokemonPageDataFetched, setPokemonPageDataFetched, selectPokemonPageData, fetchPokemonDataByIndexAsync } from '../../features/pokemon/pokemonSlice.js';
 import { NavBar } from '../../components/NavBar/NavBar.js';
 import { TypeBlock } from '../../components/TypeBlock/TypeBlock.js';
+import "./PokemonPage.css";
 
 export function PokemonPage() {
   const dispatch = useDispatch();
@@ -12,7 +13,7 @@ export function PokemonPage() {
   const pokemonPageDataFetched = useSelector(selectPokemonPageDataFetched);
   const pokemonPageData = useSelector(selectPokemonPageData);
   // const {name, url, imageUrl} = useSelector(selectAllPokemon)[number];
-  const { name, types, height, weight } = pokemonPageData;
+  const { name, generation, types, height, weight } = pokemonPageData;
   const imageUrl = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/" + number + ".png";
   
   useEffect(() => {
@@ -48,12 +49,23 @@ export function PokemonPage() {
     <div>
       <NavBar />
       <h1>{name}</h1>
-      <div className="pokemon-data-container">
+      <div className="pokemon-page-flex-container">
         <img src={imageUrl}></img>
-        <h3>Type: {types.toString()}</h3>
-        {types.map(type => <TypeBlock type={type}/>)}
-        <h3>Height: {height}m</h3>
-        <h3>Weight: {weight}kg</h3>
+        <div className="pokemon-page-data-container">
+          <h3>Number:</h3>
+          <h3>{number}</h3>
+          <h3>Generation:</h3>
+          <h3>{generation}</h3>
+          <h3>Type:</h3>
+          <div className="pokemon-page-types-container">
+            {types.map(type => <TypeBlock type={type}/>)}
+          </div>
+          <h3>Height:</h3>
+          <h3>{height}m</h3>
+          <h3>Weight:</h3>
+          <h3>{weight}kg</h3>
+          <h4 className="two-column-cell">Placeholder this is the placeholder alsdnks sjkndkjf dnf slner rmmnas wkwen w weewwe</h4>
+        </div>
       </div>
     </div>
   )
