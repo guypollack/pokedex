406e05465218bdc2125791b30a09a74287e7f7d5
Guy Pollack <guypollack@Guys-MacBook-Pro.local>
2023-03-06 16:51:14 +0200

    Create GuessBar

---
+++ src/pages/GamePage/GuessBar.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export function GuessBar() {
+  return (
+    <div>
+      <label htmlFor="guess-bar"></label>
+      <input type="text" id="guess-bar"></input>
+    </div>
+  )
+}
