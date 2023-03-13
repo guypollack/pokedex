fa869148e5eb732622cd6e6db5b68c4d31d13170
Guy Pollack <guypollack@Guys-MacBook-Pro.local>
2023-03-13 17:14:09 +0200

    Comment out console log in nameFormatter function

--- src/util/nameFormatter.js
+++ src/util/nameFormatter.js
@@ -2,7 +2,7 @@ export function nameFormatter(name) {
   if (!name) return;
   let nameString = "";
   const nameArray = name.split("-");
-  console.log(nameArray);
+  // console.log(nameArray);
   for (const elem of nameArray) {
     nameString += elem.slice(0,1).toUpperCase() + elem.slice(1) + " ";
   }
