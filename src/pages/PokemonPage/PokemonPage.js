976edb3a03ce4645e3dae4def3cb466cf6116b58
Guy Pollack <guypollack@Guys-MacBook-Pro.local>
2023-03-14 16:15:31 +0200

    Dispatch fetchAllPokemonAsync when PokemonPage is loaded and pull name from allPokemon object and increase height of .pokemon-page-loading-container
    Prevents wrong name being displayed after navigation arrows used several times in a row

--- src/pages/PokemonPage/PokemonPage.js
+++ src/pages/PokemonPage/PokemonPage.js
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { nameFormatter } from '../../util/nameFormatter.js';
-import { selectPokemonPageDataFetched, setPokemonPageDataFetched, selectPokemonPageData, fetchPokemonDataByIndexAsync, selectPokemonPageDescription, selectPokemonPageDescriptionFetched, setPokemonPageDescriptionFetched, fetchPokemonDescriptionByIndexAsync } from '../../features/pokemon/pokemonSlice.js';
+import { selectPokemonPageDataFetched, setPokemonPageDataFetched, selectPokemonPageData, fetchPokemonDataByIndexAsync, selectPokemonPageDescription, selectPokemonPageDescriptionFetched, setPokemonPageDescriptionFetched, fetchPokemonDescriptionByIndexAsync, selectAllPokemonFetched, selectAllPokemon, fetchAllPokemonAsync } from '../../features/pokemon/pokemonSlice.js';
 import { selectCurrentUser } from '../../features/users/usersSlice.js';
 import { selectFavourites } from "../../features/favourites/favouritesSlice";
 import { NavBar } from '../../components/NavBar/NavBar.js';
@@ -14,6 +14,11 @@ import "./PokemonPage.css";
 export function PokemonPage() {
   const dispatch = useDispatch();
   const { number } = useParams();
+  // let name;
+  const allPokemonFetched = useSelector(selectAllPokemonFetched);
+  const allPokemon = useSelector(selectAllPokemon);
+  // let name = "";
+  // const name = useSelector(selectAllPokemon)[number]["name"];
   const pokemonPageDataFetched = useSelector(selectPokemonPageDataFetched);
   const pokemonPageData = useSelector(selectPokemonPageData);
   const pokemonPageDescriptionFetched = useSelector(selectPokemonPageDescriptionFetched);
@@ -21,7 +26,7 @@ export function PokemonPage() {
   const user = useSelector(selectCurrentUser);
   const favourites = useSelector(selectFavourites);
   const favourited = favourites[user].includes(number);
-  const { name, generation, types, height, weight } = pokemonPageData;
+  const { generation, types, height, weight } = pokemonPageData;
   // const formattedName = nameFormatter(name);
   const imageUrl = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/" + number + ".png";
 
@@ -34,6 +39,12 @@ export function PokemonPage() {
   } else {
     favouriteButtonMessage = "Log in to add pokémon to favourites"
   }
+
+  useEffect(() => {
+    if (!allPokemonFetched) {
+      dispatch(fetchAllPokemonAsync());
+    }
+  },[allPokemonFetched])
   
   useEffect(() => {
     dispatch(setPokemonPageDataFetched(false));
@@ -43,12 +54,6 @@ export function PokemonPage() {
       dispatch(setPokemonPageDescriptionFetched(false));
     }
   },[number])
-
-  // useEffect(() => {
-  //   if (!allPokemonFetched) {
-  //     dispatch(fetchAllPokemonAsync());
-  //   }
-  // },[allPokemonFetched])
 
   useEffect(() => {
     if (!pokemonPageDataFetched) {
@@ -63,13 +68,12 @@ export function PokemonPage() {
   },[pokemonPageDescriptionFetched]);
 
   // consider deleting if statement because data is loading fast
-  if (!pokemonPageDataFetched || !pokemonPageDescriptionFetched) {
+  if (!pokemonPageDataFetched || !pokemonPageDescriptionFetched || !allPokemonFetched) {
     // alert("A");
     return (
     <div className="pokemon-page">
       <div className="pokemon-page-header">
         <NavBar />
-        <h1>{name}</h1>
       </div>
       <div className="pokemon-page-loading-container">
         <ArrowContainer side="left" number={number} />
@@ -79,40 +83,42 @@ export function PokemonPage() {
     )
   }
 
-  return (
-    <div className="pokemon-page">
-      <div className="pokemon-page-header">
-        <NavBar />
-        <h1>{name}</h1>
-      </div>
-      <div className="pokemon-page-content-container">
-        <ArrowContainer side="left" number={number} />
-        <div className="pokemon-page-grid-container">
-          <div className="pokemon-page-image-container">
-            <img src={imageUrl}></img>
-          </div>
-          <div className="pokemon-page-data-container">
-            <h3>Number:</h3>
-            <h3>{number}</h3>
-            <h3>Generation:</h3>
-            <h3>{generation}</h3>
-            <h3>Type:</h3>
-            <div className="pokemon-page-types-container">
-              {types.map(type => <TypeBlock type={type} key={`type-block-${type}`}/>)}
-            </div>
-            <h3>Height:</h3>
-            <h3>{height}m</h3>
-            <h3>Weight:</h3>
-            <h3>{weight}kg</h3>
-            <h4 className="two-column-cell description">{description}</h4>
-            <div className="two-column-cell favourite-button-container">
-              <p>{favouriteButtonMessage}</p>
-              <FavouriteButton number={number} />
-            </div>
-          </div>
+  if (allPokemonFetched) {
+    return (
+      <div className="pokemon-page">
+        <div className="pokemon-page-header">
+          <NavBar />
+          <h1>{allPokemon[number].name}</h1>
         </div>
-        <ArrowContainer side="right" number={number} />
+        <div className="pokemon-page-content-container">
+          <ArrowContainer side="left" number={number} />
+          <div className="pokemon-page-grid-container">
+            <div className="pokemon-page-image-container">
+              <img src={imageUrl}></img>
+            </div>
+            <div className="pokemon-page-data-container">
+              <h3>Number:</h3>
+              <h3>{number}</h3>
+              <h3>Generation:</h3>
+              <h3>{generation}</h3>
+              <h3>Type:</h3>
+              <div className="pokemon-page-types-container">
+                {types.map(type => <TypeBlock type={type} key={`type-block-${type}`}/>)}
+              </div>
+              <h3>Height:</h3>
+              <h3>{height}m</h3>
+              <h3>Weight:</h3>
+              <h3>{weight}kg</h3>
+              <h4 className="two-column-cell description">{description}</h4>
+              <div className="two-column-cell favourite-button-container">
+                <p>{favouriteButtonMessage}</p>
+                <FavouriteButton number={number} />
+              </div>
+            </div>
+          </div>
+          <ArrowContainer side="right" number={number} />
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
