766510a28b89e606a48a0337947c52ab43be4b35
Guy Pollack <guypollack@Guys-MacBook-Pro.local>
2023-03-20 12:48:05 +0200

    Add playStatus property which can be toggled to play/pause music. Add fontToggled property used to make music play when first toggled

--- src/features/design/designSlice.js
+++ src/features/design/designSlice.js
@@ -1,7 +1,9 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-  fontStyle: "normal"
+  fontStyle: "normal",
+  fontToggled: false,
+  playStatus: "PAUSED"
 };
 
 export const designSlice = createSlice({
@@ -9,12 +11,26 @@ export const designSlice = createSlice({
   initialState,
   reducers: {
     toggleFontStyle: (state) => {
-      state.fontStyle = state.fontStyle === "normal" ? "gameboy" : "normal";
+      if (state.fontStyle === "normal") {
+        state.fontStyle = "gameboy";
+      } else {
+        state.fontStyle = "normal";
+        state.playStatus = "PAUSED";
+      }
+      if (!state.fontToggled) {
+        state.playStatus = "PLAYING";
+      }
+      state.fontToggled = true;
+    },
+    togglePlayStatus: (state) => {
+      state.playStatus = state.playStatus === "PAUSED" ? "PLAYING" : "PAUSED";
     }
   }
 });
 
 export const selectFontStyle = (state) => state.design.fontStyle;
-export const { toggleFontStyle } = designSlice.actions;
+export const selectFontToggled = (state) => state.design.fontToggled;
+export const selectPlayStatus = (state) => state.design.playStatus;
+export const { toggleFontStyle, togglePlayStatus } = designSlice.actions;
 
 export default designSlice.reducer;
