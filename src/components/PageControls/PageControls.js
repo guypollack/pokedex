ab864b59454c25e961f0aecace3af92a2e3adc32
Guy Pollack <guypollack@Guys-MacBook-Pro.local>
2023-03-20 12:49:32 +0200

    Add play and pause buttons to PageControls to toggle music playing and add styling

--- src/components/PageControls/PageControls.js
+++ src/components/PageControls/PageControls.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Slider } from '../../components/Slider/Slider';
-import { selectFontStyle, toggleFontStyle } from '../../features/design/designSlice';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlay, faPause } from "@fortawesome/free-solid-svg-icons";
+import { selectFontStyle, toggleFontStyle, selectPlayStatus, togglePlayStatus } from '../../features/design/designSlice';
 import "./PageControls.css";
 
 export function PageControls() {
   const dispatch = useDispatch();
 
   const fontStyle = useSelector(selectFontStyle);
+  const playStatus = useSelector(selectPlayStatus);
 
   function handleFontChange() {
     dispatch(toggleFontStyle());
@@ -19,9 +22,15 @@ export function PageControls() {
     }
   }
 
+  function handleClickPlayPause() {
+    dispatch(togglePlayStatus());
+  }
+
   return (
     <div className="page-controls">
       <Slider heading="" value1="Normal" value2="Gameboy Mode" checked={fontStyle === "gameboy"} onChange={handleFontChange} />
+      {(fontStyle === "gameboy" && playStatus === "PAUSED") && <FontAwesomeIcon className="play-pause-button" icon={faPlay} onClick={handleClickPlayPause} />}
+      {(fontStyle === "gameboy" && playStatus === "PLAYING") && <FontAwesomeIcon className="play-pause-button" icon={faPause} onClick={handleClickPlayPause} />}
     </div>
   )
 
