05da332077ae3143b57b1a32f252c0adfb6036ac
Guy Pollack <guypollack@Guys-MacBook-Pro.local>
2023-03-23 11:31:56 +0200

    Replace fontSwitches property with nonGameFontSwitches, to accurately work out when to play GameboyStartupSound

--- src/features/design/designSlice.js
+++ src/features/design/designSlice.js
@@ -2,7 +2,7 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
   fontStyle: "normal",
-  fontSwitches: 0,
+  nonGameFontSwitches: 0,
   playStatus: "PAUSED"
 };
 
@@ -15,17 +15,19 @@ export const designSlice = createSlice({
       if (state.fontSwitches === 0) {
         state.playStatus = "PLAYING";
       }
-      state.fontSwitches++;
     },
     togglePlayStatus: (state) => {
       state.playStatus = state.playStatus === "PAUSED" ? "PLAYING" : "PAUSED";
+    },
+    incrementNonGameFontSwitches: (state) => {
+      state.nonGameFontSwitches++;
     }
   }
 });
 
 export const selectFontStyle = (state) => state.design.fontStyle;
 export const selectPlayStatus = (state) => state.design.playStatus;
-export const selectFontSwitches = (state) => state.design.fontSwitches;
-export const { toggleFontStyle, togglePlayStatus } = designSlice.actions;
+export const selectNonGameFontSwitches = (state) => state.design.nonGameFontSwitches;
+export const { toggleFontStyle, togglePlayStatus, incrementNonGameFontSwitches } = designSlice.actions;
 
 export default designSlice.reducer;
