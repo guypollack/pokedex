918e15533b68c063090358420cdcb9891b814d6a
Guy Pollack <guypollack@Guys-MacBook-Pro.local>
2023-03-23 16:34:18 +0200

    Remove properties checking number of toggles/switches and replace with gameboySoundPlayed property and reducer

--- src/features/design/designSlice.js
+++ src/features/design/designSlice.js
@@ -2,8 +2,8 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
   fontStyle: "normal",
-  nonGameFontSwitches: 0,
-  playStatus: "PLAYING"
+  playStatus: "PLAYING",
+  gameboySoundPlayed: false
 };
 
 export const designSlice = createSlice({
@@ -12,22 +12,19 @@ export const designSlice = createSlice({
   reducers: {
     toggleFontStyle: (state) => {
       state.fontStyle = state.fontStyle === "normal" ? "gameboy" : "normal";
-      // if (state.fontSwitches === 0) {
-      //   state.playStatus = "PLAYING";
-      // }
     },
     togglePlayStatus: (state) => {
       state.playStatus = state.playStatus === "PAUSED" ? "PLAYING" : "PAUSED";
     },
-    incrementNonGameFontSwitches: (state) => {
-      state.nonGameFontSwitches++;
+    setGameboySoundPlayed: (state, action) => {
+      state.gameboySoundPlayed = action.payload;
     }
   }
 });
 
 export const selectFontStyle = (state) => state.design.fontStyle;
 export const selectPlayStatus = (state) => state.design.playStatus;
-export const selectNonGameFontSwitches = (state) => state.design.nonGameFontSwitches;
-export const { toggleFontStyle, togglePlayStatus, incrementNonGameFontSwitches } = designSlice.actions;
+export const selectGameboySoundPlayed = (state) => state.design.gameboySoundPlayed;
+export const { toggleFontStyle, togglePlayStatus, setGameboySoundPlayed } = designSlice.actions;
 
 export default designSlice.reducer;
