51dacce88e7fe7a25ca612a16eadac1b2726b954
Guy Pollack <guypollack@Guys-MacBook-Pro.local>
2023-03-24 17:45:57 +0300

    Add ALL/ANY Slider to each FilterBlocksContainer to toggle search type within each category individually

--- src/components/FilterBlocksContainer/FilterBlocksContainer.js
+++ src/components/FilterBlocksContainer/FilterBlocksContainer.js
@@ -1,13 +1,19 @@
 import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { selectInCategorySearchTypes, toggleSearchTypeByCategory } from "../../features/pokemon/pokemonSlice";
 import { nameFormatter } from "../../util/nameFormatter";
-
+import { Slider } from "../Slider/Slider";
 import { FilterBlock } from "../FilterBlock/FilterBlock";
 import "./FilterBlocksContainer.css";
 
 export function FilterBlocksContainer({category, filters}) {
+  const dispatch = useDispatch();
+  const searchType = useSelector(selectInCategorySearchTypes)[category + "s"];
+
   return (
     <div className="filter-blocks-container">
       <h3>{nameFormatter(category)}</h3>
+      <Slider heading={`${category} slider`} value1="ALL" value2="ANY" checked={searchType === "or"} onChange={() => dispatch(toggleSearchTypeByCategory(category + "s"))} />
       <div className="filter-blocks-flex-container">
         {filters.map(([cat, value], index) => <FilterBlock property={cat} value={value} filterNumber={index} key={`${cat}-filter-block-${index}`}/>)}
       </div>
