1337a8b42cf7f26a6182a067fc44aab8c8390e17
Guy Pollack <guypollack@Guys-MacBook-Pro.local>
2023-03-26 12:59:17 +0300

    Correct fromCharCode method in stringMinusChar12 assignment

--- src/util/removeInvisibles.js
+++ src/util/removeInvisibles.js
@@ -1,5 +1,5 @@
 export function removeInvisibles(string) {
   const stringMinusChar10 = string.split(String.fromCharCode(10)).join(" ");
-  const stringMinusChar12 = stringMinusChar10.split(String.fromCharCode(10)).join(" ");
+  const stringMinusChar12 = stringMinusChar10.split(String.fromCharCode(12)).join(" ");
   return stringMinusChar12;
 }
